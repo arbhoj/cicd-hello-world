14197327f5c9b1d34413ffb0b8b424b64df8b8f9
Arvind Bhoj <arbhoj@d2iq.com>
2021-01-13 13:41:40 -0500

    Made some minor changes to the app

--- main.go
+++ main.go
@@ -56,7 +56,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: %s\n", Version)
 	fmt.Fprintf(w, "Build time: %s\n", Buildtime)
-
 	fmt.Fprintf(w, "This change will trigger a build test!\n")
 }
 
